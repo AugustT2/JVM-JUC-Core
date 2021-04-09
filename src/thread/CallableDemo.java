61a25d56275489e05dddebb8649f79d02ffae290
AugustT2 <killerman20150307@gmail.com>
2021-04-09 20:50:55 +0800

    call接口只FutureTask

--- src/thread/CallableDemo.java
+++ src/thread/CallableDemo.java
@@ -9,6 +9,7 @@ public class CallableDemo {
         FutureTask<Integer> futureTask = new FutureTask<>(new MyThread());
         new Thread(futureTask, "AA").start();
         int result01 = 100;
+        //get尽量方法后面，不然如果线程没有执行完，会一直阻塞等线程执行完了再去取返回值，然后在往下执行代码。
         int result02 = futureTask.get();
         System.out.println("result=" + (result01 + result02));
     }
