17ddeb5e7ec9556454d9c37b749c8dadd1e401f7
Elaine Ding <Elaine.Ding@nike.com>
2021-04-12 16:48:33 +0800

    Exception需要改成Throwable，因为产生的Error，不会被Execption caught住，而无法打印下面的i

--- src/jvm/GCOverheadDemo.java
+++ src/jvm/GCOverheadDemo.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 //-Xms10m -Xmx10m -XX:MaxDirectMemorySize=5m -XX:+PrintGCDetails
+//加上启动参数：-Xms10m -Xmx10m -XX:+PrintGCDetails -XX:MaxDirectMemorySize=5M
 public class GCOverheadDemo {
     public static void main(String[] args) {
         int i = 0;
@@ -12,8 +13,8 @@ public class GCOverheadDemo {
             while (true) {
                 list.add(String.valueOf(++i).intern());
             }
-        } catch (Exception e) {
-            System.out.println("************i" + i);
+        } catch (Throwable e) {
+            System.out.println("************i=" + i);
             e.printStackTrace();
             throw e;
         }
