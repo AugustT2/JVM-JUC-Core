8bb22d27fb7707c13c8ee3a3e5c8c5b54a11bc35
AugustT2 <killerman20150307@gmail.com>
2021-04-15 22:19:35 +0800

    learn 集合

---
+++ src/collection_study/Collection_.java
@@ -0,0 +1,21 @@
+package collection_study;
+
+import java.util.*;
+
+public class Collection_ {
+    public static void main(String[] args) {
+//        Collection
+        List<String> list = Arrays.asList("aaa", "bbb", "ccc");
+//        list.add("ddd");
+//        System.out.println(list);
+        Iterator<String> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+//        iterator.next();
+        for (String s : list) {
+            System.out.println(s);
+        }
+    }
+
+}
