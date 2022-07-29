65e3a00ce60fe1816a04336fcdc999394a2d8b82
AugustT2 <killerman20150307@gmail.com>
2022-07-29 14:46:51 +0800

    1.将list等分成n个list 2.java读取src下的配置文件（推荐ResourceBoundle）

---
+++ src/ListUtil.java
@@ -0,0 +1,80 @@
+import javase.iodemo.FileInputStream_;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.*;
+
+
+public class ListUtil {
+    public static void main(String[] args) throws IOException {
+
+        List<Integer> integers=new ArrayList<>();
+        integers.add(1);
+        integers.add(2);
+        integers.add(3);
+        integers.add(4);
+        integers.add(5);
+        integers.add(6);
+        integers.add(7);
+        integers.add(8);
+        integers.add(9);
+        integers.add(10);
+        integers.add(11);
+        integers.add(12);
+        integers.add(13);
+        integers.add(14);
+        integers.add(15);
+        integers.add(16);
+        List<List<Integer>> lists=averageAssign(integers, 7);
+        System.out.println(lists);
+
+        String abc = "A_B_C_6";
+        System.out.println(abc.substring(abc.length()-1));
+
+        //方法一，获取绝对路径后再io流操作
+        String path=Thread.currentThread().getContextClassLoader().getResource("mysql.properties").getPath();
+        System.out.println("path:"+ path);
+        FileInputStream fileInputStream = new FileInputStream(path);
+        BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
+        Properties properties = new Properties();
+        properties.load(bufferedInputStream);
+        System.out.println(properties.get("ip"));
+
+        //方法二，利用ResourceBundle， 注意配置文件需要以properties结尾，传进去的参数不要带properties
+        ResourceBundle bundle=ResourceBundle.getBundle("mysql", Locale.ENGLISH);
+        String className=bundle.getString("ip");
+        System.out.println(className);
+
+
+    }
+
+
+
+
+
+    /**
+     * 将一个list均等分成n个list,主要通过偏移量来实现的
+     * @param source
+     * @return
+     */
+    public static <T> List<List<T>> averageAssign(List<T> source, int n){
+        List<List<T>> result=new ArrayList<List<T>>();
+        int remaider=source.size()%n;  //(先计算出余数)
+        int number=source.size()/n;  //然后是商
+        int offset=0;//偏移量
+        for(int i=0;i<n;i++){
+            List<T> value=null;
+            if(remaider>0){
+                value=source.subList(i*number+offset, (i+1)*number+offset+1);
+                remaider--;
+                offset++;
+            }else{
+                value=source.subList(i*number+offset, (i+1)*number+offset);
+            }
+            result.add(value);
+        }
+        return result;
+    }
+}
