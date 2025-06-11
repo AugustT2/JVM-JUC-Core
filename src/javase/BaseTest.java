77c0ac457ee90ca4e45bfe1096a5791e9315dbfa
AugustT2 <killerman20150307@gmail.com>
2025-06-11 15:37:25 +0800

    弱引用案例增强

---
+++ src/javase/BaseTest.java
@@ -0,0 +1,39 @@
+package javase;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Hashtable;
+
+public class BaseTest {
+    public static void main(String[] args) {
+        int i = 1;
+//        System.out.println(i instanceof Integer );
+//        System.out.println(i instanceof Object );
+//        Integer i1 = new Integer(1);
+//        System.out.println(i1 instanceof Integer);
+//        System.out.println(i1 instanceof Object);
+//        System.out.println(null instanceof Object);
+
+        //IntegerCache -128~127, 可以自定义最大值-XX:AutoBoxCacheMax=500，参考源码
+        Integer i1 = 100;
+        Integer i2 = 100;
+        Integer i3 = 200;
+        Integer i4 = 200;
+
+        System.out.println(i1==i2);  //true
+        System.out.println(i3==i4);  //false
+
+        Double d1 = 1.0;
+
+        Object object = null;
+        System.out.println(new Object().equals(object));
+        boolean equalsNull = object.equals(new Object());
+        System.out.println(equalsNull);
+
+        ArrayList<Object> list = new ArrayList<>();
+
+        HashMap<Object, Object> map = new HashMap<>();
+        Hashtable<String, String> table = new Hashtable<>();
+
+    }
+}
--- src/jvm/WeakHashMapDemo.java
+++ src/jvm/WeakHashMapDemo.java
@@ -8,6 +8,8 @@ public class WeakHashMapDemo {
         myHashMap();
         System.out.println("===============");
         myWeakHashMap();
+        System.out.println("===============");
+        myWeakHashMap2();
     }
 
     private static void myHashMap() {
@@ -34,6 +36,45 @@ public class WeakHashMapDemo {
         System.out.println(map);
 
         System.gc();
+        System.out.println(map + "\t" + map.size());
+    }
+
+    /**
+     * 我理解你的疑问。在
+     * WeakHashMap
+     *  中，即使你将 key 设置为 null 并调用了 System.gc()，
+     * WeakHashMap
+     *  的 size() 仍然显示为 1，这看起来似乎不符合弱引用的预期行为。让我解释一下原因：
+     *
+     * 自动装箱的缓存：在你的代码中，你使用了 Integer key = 2。Java 会对小整数（-128 到 127）进行缓存，这意味着 Integer.valueOf(2) 会返回缓存中的对象，而不是创建新对象。
+     * 强引用仍然存在：
+     * WeakHashMap
+     *  的弱引用是针对 key 的，而不是 value。虽然你将 key 变量设置为 null，但是 Integer 对象 2 可能仍然被 JVM 的整数缓存所引用。
+     * 垃圾回收的不确定性：System.gc() 只是建议 JVM 进行垃圾回收，但不保证立即执行。即使执行了，由于整数缓存的原因，Integer(2) 对象可能仍然有强引用。
+     * 要看到
+     * WeakHashMap
+     *  的预期行为，可以尝试以下修改：
+     */
+    private static void myWeakHashMap2() {
+        WeakHashMap<Integer, String> map = new WeakHashMap<>();
+        // 使用 new Integer() 来避免使用缓存
+        Integer key = new Integer(1000);  // 使用一个大数字，不在缓存范围内
+        String value = "WeakHashMap";
+        map.put(key, value);
+        System.out.println(map);
+
+        key = null;
+        System.out.println(map);
+
+        // 尝试强制垃圾回收
+        System.gc();
+        // 给 GC 一些时间
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
         System.out.println(map + "\t" + map.size());
     }
 }
